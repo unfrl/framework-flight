22d8b6216e34ada28fbef2bf5b88e072c7f58f7c
andrewnoyes <andrew@noyes.io>
2022-02-02 14:06:00 -0700

    formatting

--- gin_cats/cats.go
+++ gin_cats/cats.go
@@ -18,6 +18,24 @@ type UpsertCat struct {
 	Color string `json:"color" binding:"required"`
 }
 
+func CreateCat(db *gorm.DB) gin.HandlerFunc {
+	return func(context *gin.Context) {
+		var json UpsertCat
+		if err := context.ShouldBindJSON(&json); err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		var cat = Cat{Name: json.Name, Color: json.Color}
+		if err := db.Create(&cat).Error; err != nil {
+			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		context.JSON(http.StatusCreated, gin.H{"result": cat})
+	}
+}
+
 func GetCats(db *gorm.DB) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		var cats []Cat
@@ -34,9 +52,7 @@ func GetCat(db *gorm.DB) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		var cat Cat
 
-		result := db.Find(&cat, context.Param("id"))
-
-		if result.RowsAffected == 0 {
+		if result := db.Find(&cat, context.Param("id")); result.RowsAffected == 0 {
 			context.JSON(http.StatusNotFound, gin.H{
 				"error": "Cat not found",
 			})
@@ -46,23 +62,5 @@ func GetCat(db *gorm.DB) gin.HandlerFunc {
 		context.JSON(http.StatusOK, gin.H{
 			"result": cat,
 		})
-	}
-}
-
-func CreateCat(db *gorm.DB) gin.HandlerFunc {
-	return func(context *gin.Context) {
-		var json UpsertCat
-		if err := context.ShouldBindJSON(&json); err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		var cat = Cat{Name: json.Name, Color: json.Color}
-		if err := db.Create(&cat).Error; err != nil {
-			context.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		context.JSON(http.StatusCreated, gin.H{"result": cat})
 	}
 }
