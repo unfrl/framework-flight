d9ac2e47f5d830ce021dce5c9b447baf447be179
andrewnoyes <andrew@noyes.io>
2022-02-02 11:15:48 -0700

    more gin cats

--- gin_cats/gin_cats.go
+++ gin_cats/gin_cats.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	fmt.Println("gin cats go!")
+
+	engine := gin.Default()
+
+	engine.GET("/cats", func(context *gin.Context) {
+		context.JSON(200, gin.H{
+			"cats": "meow",
+		})
+	})
+
+	engine.Run() // default port is 8080
 }
