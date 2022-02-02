83f4b23d66205d62dd974e0bc572a9d18bfcf2ed
andrewnoyes <andrew@noyes.io>
2022-02-02 12:54:18 -0700

    read cats from db

--- gin_cats/gin_cats.go
+++ gin_cats/gin_cats.go
@@ -4,22 +4,44 @@ import (
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+const DbSource string = "host=localhost user=postgres password=postgres dbname=phoenix_cats_dev"
+
 type Cat struct {
-	Name  string `json:"name"`
-	Color string `json:"color"`
+	gorm.Model
+	Name  string
+	Color string
 }
 
 func main() {
 	fmt.Println("gin cats go!")
 
+	db, err := gorm.Open(
+		postgres.New(postgres.Config{
+			DSN:                  DbSource,
+			PreferSimpleProtocol: true,
+		}), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect to db!")
+	}
+
+	db.AutoMigrate(&Cat{})
+
+	if db.Find(&Cat{}).RowsAffected == 0 {
+		db.Create(&Cat{Name: "Benny", Color: "Brown"})
+	}
+
 	router := gin.Default()
 
 	router.GET("/cats", func(context *gin.Context) {
-		cats := []Cat{
-			{Name: "Benny", Color: "Brown"},
-		}
+		var cats []Cat
+
+		db.Find(&cats)
+
 		context.JSON(200, gin.H{
 			"cats": cats,
 		})
