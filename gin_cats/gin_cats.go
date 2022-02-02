c6706535947c9ae241b499178a304f0c3afcc8c4
andrewnoyes <andrew@noyes.io>
2022-02-02 11:26:31 -0700

    cat struct

--- gin_cats/gin_cats.go
+++ gin_cats/gin_cats.go
@@ -6,16 +6,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Cat struct {
+	Name  string `json:"name"`
+	Color string `json:"color"`
+}
+
 func main() {
 	fmt.Println("gin cats go!")
 
 	engine := gin.Default()
 
 	engine.GET("/cats", func(context *gin.Context) {
+		cats := []Cat{
+			{Name: "Benny", Color: "Brown"},
+		}
 		context.JSON(200, gin.H{
-			"cats": "meow",
+			"cats": cats,
 		})
 	})
 
-	engine.Run() // default port is 8080
+	engine.Run() // default localhost:8080
 }
